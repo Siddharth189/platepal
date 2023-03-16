55427fbac37eb1906ecd196ba40b4b6048a41348
siddharth189 <siddharthiiitg@gmail.com>
2023-03-16 12:24:54 +0530

    fix cors 1

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,7 @@
 // Put all the hardcoded data heres
 
 export const DESKTOP_WEB_LISTING =
-  "api/restaurants/list/v5?lat=26.1756772&lng=91.7417143&page_type=DESKTOP_WEB_LISTING";
+  "/api/restaurants/list/v5?lat=26.1756772&lng=91.7417143&page_type=DESKTOP_WEB_LISTING";
 
 export const IMG_CLOUD_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
