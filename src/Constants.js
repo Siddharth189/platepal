b6ce8afed9eaf8c8df0796075f4fac6a4047923f
siddharth189 <siddharthiiitg@gmail.com>
2023-03-18 12:48:22 +0530

    fix cors

--- src/Constants.js
+++ src/Constants.js
@@ -1,12 +1,14 @@
 // Put all the hardcoded datas here
+const CORS_PROXY_URL = "/api/";
 
 export const DESKTOP_WEB_LISTING =
-  "https://fooodvilla.netlify.app/api//dapi/restaurants/list/v5?page_type=DESKTOP_WEB_LISTING&lat=26.1756772&lng=91.7417143";
+  CORS_PROXY_URL +
+  "/dapi/restaurants/list/v5?lat=26.1756772&lng=91.7417143&page_type=DESKTOP_WEB_LISTING";
 
 export const IMG_CLOUD_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const FETCH_RESTRAUNT_URL =
-  "https://fooodvilla.netlify.app/api//dapi/menu/v4/full?lat=26.1790101&lng=91.7408019&menuId=";
+  "https://www.swiggy.com/dapi/menu/v4/full?lat=26.1790101&lng=91.7408019&menuId=";
 
 export const restrauntList = [];
