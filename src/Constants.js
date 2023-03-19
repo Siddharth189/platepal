28f302d5321056426f9d0753a096616181ef442c
siddharth189 <siddharthiiitg@gmail.com>
2023-03-19 21:46:13 +0530

    fix cors

--- src/Constants.js
+++ src/Constants.js
@@ -8,6 +8,6 @@ export const IMG_CLOUD_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const FETCH_RESTRAUNT_URL =
-  CORS_PROXY_URL + "/dapi/menu/v4/full?lat=26.1790101&lng=91.7408019&menuId=";
+  "https://www.swiggy.com//dapi/menu/v4/full?lat=26.1790101&lng=91.7408019&menuId=";
 
 export const restrauntList = [];
