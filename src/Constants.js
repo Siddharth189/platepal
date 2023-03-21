4eda3452a7f3f145589cdd5b386fa7449edf1483
siddharth189 <siddharthiiitg@gmail.com>
2023-03-21 11:04:02 +0530

    set up a proxy server in a React app

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,4 @@
 // Put all the hardcoded datas here
-const corsProxyUrl = "https://cors-anywhere.herokuapp.com/";
 
 export const DESKTOP_WEB_LISTING =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.1756772&lng=91.7417143&page_type=DESKTOP_WEB_LISTING";
@@ -8,7 +7,7 @@ export const IMG_CLOUD_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const FETCH_RESTRAUNT_URL =
-  corsProxyUrl +
-  "https://www.swiggy.com//dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.1756772&lng=91.7417143&restaurantId=";
+  CORS_PROXY_URL +
+  "/api/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.1756772&lng=91.7417143&restaurantId=";
 
 export const restrauntList = [];
