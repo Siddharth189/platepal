15585fff9064f9e964111bd6259a29d4cf7d59f6
siddharth189 <siddharthiiitg@gmail.com>
2023-03-22 12:11:01 +0530

    fixed cors

--- src/Constants.js
+++ src/Constants.js
@@ -7,6 +7,6 @@ export const IMG_CLOUD_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const FETCH_RESTRAUNT_URL =
-  "/api/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.1756772&lng=91.7417143&restaurantId=";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.1756772&lng=91.7417143&restaurantId=";
 
 export const restrauntList = [];
